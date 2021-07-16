e140ccb8e6bb4ff0001c57078ba653d5b4193347
SergeyKulnin <stikboss@mail.ru>
2021-07-16 14:52:25 +1000

    Update remotepipe.groovy

---
+++ remotepipe.groovy
@@ -0,0 +1,15 @@
+pipeline{
+    agent { 
+        label 'user1-virtual-machine' 
+        }
+echo "Название сборки: $BUILD_DISPLAY_NAME"
+sleep 5
+echo "Номер сборки: $BUILD_NUMBER"
+sleep 7
+whoami
+sleep 5
+ip adress
+hostname
+sleep 7
+
+}
