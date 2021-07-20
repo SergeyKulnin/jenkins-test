b9363b863ecc7e5c453c1ffbd223b86e4b0c9201
SergeyKulnin <stikboss@mail.ru>
2021-07-20 16:56:55 +1000

    Update remotepipe.groovy

--- remotepipe.groovy
+++ remotepipe.groovy
@@ -7,7 +7,8 @@ pipeline {
         stage('Connection test') {
 
             steps { script {
-                sleep 7
+                timestamps{
+                    sleep 7
                 
                 sh '''#!/bin/bash
                  whoami
@@ -17,22 +18,26 @@ pipeline {
                  hostname 
          '''
                 sleep 7
+                }
                         }}
 
         }
 
         stage('Execute script'){
             steps { script {
-                
+                timestamps{
                 sleep 10
+                }
             }}
         }
 
          stage('Bash test'){
             steps { 
+                timestamps{
                 sh '''#!/bin/bash
                  sh "/home/user1/jenkins_script/jen.sh" 
          '''
+                }
             }
         }
     }    
