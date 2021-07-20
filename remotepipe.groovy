cc94494879d8380def9cbf895c61646b1d9c71c5
SergeyKulnin <stikboss@mail.ru>
2021-07-20 17:04:56 +1000

    Update remotepipe.groovy

--- remotepipe.groovy
+++ remotepipe.groovy
@@ -11,10 +11,14 @@ pipeline {
                     sleep 7
                 
                 sh '''#!/bin/bash
+                echo -en "\033[37;1;41m Информация \033[0m \n"
+                echo -en "\033[36;1;47m Пользователь \033[0m \n"
                  whoami
                  sleep 4
+                 echo -en "\033[36;1;47m IP адрес \033[0m \n"
                  ip address
                  sleep 4
+                 echo -en "\033[36;1;47m Host \033[0m \n"
                  hostname 
          '''
                 sleep 7
