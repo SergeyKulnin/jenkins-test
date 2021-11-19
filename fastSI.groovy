f489c9870e83580999c4198541facdb36a8ea6ae
SergeyKulnin <stikboss@mail.ru>
2021-11-19 15:10:21 +1000

    Update fastSI.groovy

--- fastSI.groovy
+++ fastSI.groovy
@@ -3,12 +3,12 @@ import java.util.concurrent.Future
 
 class EkpDb {
 
-    Boolean executeStepList(String stepsYamlFile, String stepsList){
+    Boolean executeStepList(String stepsYamlFile){
         Boolean success
 stepsYamlFile = "stepsSI.yml"
         Yaml yamlParser = new Yaml()
         def stepsYaml = yamlParser.load((stepsYamlFile as File).text)
-        stepsYaml.split(',').each { step ->
+        stepsYaml.each { step ->
             if (stepsYaml[step].type.toString().toUpperCase().equals('PLSQL')){
                 def stepDescription = stepsYaml[step].description.toString()
                 def stepQuery = stepsYaml[step].query.toString()    
