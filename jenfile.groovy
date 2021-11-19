32e69bebbc807626d08577b329002a414f23c060
SergeyKulnin <stikboss@mail.ru>
2021-11-19 15:30:32 +1000

    Update jenfile.groovy

--- jenfile.groovy
+++ jenfile.groovy
@@ -9,17 +9,19 @@ pipeline {
             steps { script {
                 sleep 7     
 
-                stepsYamlFile = "stepsSI.yml"
+stepsYamlFile = "stepsSI.yml"
         Yaml yamlParser = new Yaml()
         def stepsYaml = yamlParser.load((stepsYamlFile as File).text)
         stepsYaml.each { step ->
             if (stepsYaml[step].type.toString().toUpperCase().equals('PLSQL')){
                 def stepDescription = stepsYaml[step].description.toString()
                 def stepQuery = stepsYaml[step].query.toString()                                
-            }
+            }}
             println stepQuery
+                
                 sleep 7
-                        }}
+                        }
+                        }
         }
     }  
 
